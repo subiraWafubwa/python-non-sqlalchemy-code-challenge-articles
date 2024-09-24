47e4f177fdabbe12bfb4386606b2b08b77de8941
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-09-24 12:13:40 +0300

    Passed Tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,100 @@
+
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
+        if not (5 <= len(title) <= 50):
+            raise ValueError("Title length must be between 5 and 50 characters inclusive")
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title attribute is immutable")
+
+    @title.deleter
+    def title(self):
+        raise AttributeError("Title attribute cannot be deleted")
+
+
+
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
 
-    def articles(self):
-        pass
+    @property
+    def name(self):
+        return self._name
 
-    def magazines(self):
-        pass
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Cannot modify the name attribute")
 
     def add_article(self, magazine, title):
-        pass
-
-    def topic_areas(self):
-        pass
-
-class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
     def articles(self):
-        pass
+        return self._articles
+
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+
+    def topic_areas(self):
+        return list(set(article.magazine.category for article in self._articles))
+
+
+  
+
+class Magazine:
+    all = []
+
+    def __init__(self, name, category):
+        self._name = name
+        self._category = category
+        self._articles = []
+        self._contributors = []
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Cannot modify the name attribute")
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        raise AttributeError("Cannot modify the category attribute")
+
+    def add_article(self, article):
+        self._articles.append(article)
+
+    def articles(self):
+        return self._articles
+
+    def add_contributor(self, author):
+        self._contributors.append(author)
 
     def contributors(self):
-        pass
+        return self._contributors
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        return [article.author for article in self._articles]
